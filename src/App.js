778c2bae4e5f1d76582b246ad49be2c018808802
vadym_makhonin <vadym_makhonin@epam.com>
2017-08-20 08:18:58 +0300

    added redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state -1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counter);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {counter: store.getState()};
+
+    store.subscribe(() => {
+      this.setState({counter: store.getState()});
+    });
+  }
+
+  increment() {
+    store.dispatch({type: 'INCREMENT'});
+  }
+
+  decrement() {
+    store.dispatch({type: 'DECREMENT'});
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,12 +41,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          {this.state.counter}
+          <button onClick={this.increment}>+</button>
+          <button onClick={this.decrement}>-</button>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
